1e6dc9e314c64c9ae39c413b13c69ac2c4ae437d
João Martins <joaogasparlopesmartins@gmail.com>
2025-10-27 13:56:57 +0000

    feat: model for table "resource" in db

---
+++ src/main/java/joaoglmartins/helloworld/model/Resource.java
@@ -0,0 +1,34 @@
+package joaoglmartins.helloworld.model;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "resource")
+public class Resource {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, length = 30)
+    private String name;
+
+    public Resource(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
